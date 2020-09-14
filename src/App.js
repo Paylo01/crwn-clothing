376da2357db212432c5159f552d116a829bc9491
gangelo <experator@outlook.fr>
2020-09-14 08:52:28 +0100

    updating our app with a shop page, preview collection component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
 import {HomePage} from './pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+  
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+const PreviewCollection = ({title, items}) => (
+    <div className="preview-collection">
+        <h1>{title.toUpperCase()}</h1>
+        <div className="preview">
+            {items.filter((item, idx) => idx < 4 ).map(({id, ...otherItemProps}) => (
+                <CollectionItem key={id} {...otherItemProps} />
+            ))}
+        </div>
+    </div>
+)
+
+export default PreviewCollection;
